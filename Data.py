0b7266aebd45c9881c9abd1a8e0e524dc9f86906
dmitry <ddimon27.03@gmail.com>
2023-09-21 21:42:52 +0300

    Add classification to set y data

--- Data.py
+++ Data.py
@@ -4,21 +4,18 @@ import numpy as np
 import os
 import random
 import pandas as pd
+import sklearn.cluster as clus
 
-ARGS_COUNT = 6
+SAVE_PATH = 'path :)'
+
+ARGS_COUNT = 5
 GENERATION_COUNT = 1000
 
 MORNING_BUSY_HOUR_START = 7
 MORNING_BUSY_HOUR_END = 10
 
 EVENING_BUSY_HOUR_START = 17
-EVENING_BUSY_HOUR_END = 19
-        
-def generate_auto_business(time: int) -> int:
-    if (MORNING_BUSY_HOUR_START <=  time <= MORNING_BUSY_HOUR_END) or (EVENING_BUSY_HOUR_START <=  time <= EVENING_BUSY_HOUR_END):
-        return random.randint(6,10)
-    else:
-        return random.randint(1,5)
+EVENING_BUSY_HOUR_END = 19 
 
 def time_function(x: int) -> float:
     k = (-abs( (0.6*x + 8)**2 - 20*x)) + 12
@@ -34,22 +31,39 @@ def generate_transport_time():
     # сколько видов транспорта будет
     # transport_counter = random.randint(1,4)
     
-    auto = random.randint(0,180) * ( random.randint(0,1) & random.randint(0,1) )
-    walk = random.randint(0,80) * ( random.randint(0,1) & random.randint(0,1) )
+    auto = random.randint(0,120) * random.randint(0,1)
+    walk = random.randint(0,80) * random.randint(0,1)
     underground = random.randint(0,100) * ( random.randint(0,1) & random.randint(0,1) )
     trol = random.randint(0,100) * ( random.randint(0,1) & random.randint(0,1) )
-    
+    if auto == 0 and walk == 0 and underground == 0 and trol == 0:
+        walk = 5
     return auto, walk, underground, trol
 
-x_dataset = np.zeros((GENERATION_COUNT, ARGS_COUNT), dtype=int)
-y_dataset = np.zeros((GENERATION_COUNT, 1), dtype=int)
+def main():
+    x_dataset = np.zeros((GENERATION_COUNT, ARGS_COUNT), dtype=int)
+    y_dataset = np.zeros((GENERATION_COUNT, 1), dtype=int)
+    
+    for i in range(GENERATION_COUNT):
+        time = random.randint(0,23)
+        day = random.randint(1,7)
+        a,w,u,t = generate_transport_time()
+        x_dataset[i] = np.array([generate_k(day, time), a, w, u, t])
+    x_dataset[:, 0] *= 100
+    x_table = pd.DataFrame(x_dataset, columns=['K', 'Auto(m)', 'Walk(m)', 'Underground(m)', 'Troleybus(m)'], dtype=float)
+    print(x_table["K"].mean(), x_table["Auto(m)"].mean(), x_table["Walk(m)"].mean())
+    
+    means = clus.MiniBatchKMeans(5, n_init=10, random_state=0).fit(x_table)
+    for i in range(GENERATION_COUNT):
+        y_dataset[i] = means.predict([x_dataset[i]])
+     
+    y_table = pd.DataFrame(y_dataset, columns=["Answer"])
+    main_p = pd.concat([x_table, y_table], axis=1)
+    
+    print(main_p[:50])
+    
+    main_p.to_csv(SAVE_PATH)
 
-for i in range(GENERATION_COUNT):
-    time = random.randint(0,23)
-    day = random.randint(1,7)
-    a,w,u,t = generate_transport_time()
-    x_dataset[i] = np.array([generate_k(day, time), a, w, u, t, generate_auto_business(time)])
+    
 
-table = pd.DataFrame(x_dataset, columns=['K', 'Auto(m)', 'Walk(m)', 'Underground(m)', 'Troleybus(m)', 'Road business'])
-print(table)
-# print(np.average( table["Auto(m)"]))
+if __name__ == "__main__":
+    main()
